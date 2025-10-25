4692e44c5462e0b8121e9a121a70d22e4a68aff8
azmira-khatun <azmiragrn@gmail.com>
2025-10-25 09:05:11 +0600

    first commit

---
+++ example-app/app/Http/Controllers/OneToManyController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\Models\Comment;
+
+class OneToManyController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::with('comments')->get();
+        return $posts;
+                // return view('index', compact('posts'));
+
+    }
+}
--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\OneToOneController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostCommentController;
+use App\Http\Controllers\OneToManyController;
 use App\Http\Controllers\HasOneThroughController;
 use App\Http\Controllers\HasManyThroughController;
 /*
@@ -41,50 +41,23 @@ Route::get('/manage', function () {
 Route::get('/master', function () {
     return view('master');
 });
-
-
-
-
-
-
-
-/// --- 2. One-to-One Profile Management Routes ---
-
-// 1. প্রোফাইল তালিকা দেখানোর জন্য রুট (Index)
+// --- 2. One-to-One Profile Management Routes ---
 Route::get('/profile', [OneToOneController::class, 'show'])->name('profile.show');
 
-// 2. প্রোফাইল তৈরির ফর্ম দেখানোর জন্য রুট (Create Form)
-Route::get('/profile/create', [OneToOneController::class, 'create'])->name('profile.create');
+Route::get('/profile/{id}/edit', [OneToOneController::class, 'edit'])->name('profile.edit');
 
-// 3. **নতুন সংযোজন:** ফর্ম থেকে ডেটা সেভ করার জন্য রুট (Store)
-Route::post('/profile', [OneToOneController::class, 'store'])->name('profile.store'); // **এটি যোগ করতে হবে**
-
-// 4. একটি নির্দিষ্ট প্রোফাইল দেখার জন্য রুট (View/Show Singular)
-Route::get('/profile/{id}', [OneToOneController::class, 'view'])->name('profile.view');
-
-// 5. **নতুন সংযোজন:** প্রোফাইল এডিট করার ফর্ম দেখানোর জন্য রুট (Edit Form)
-Route::get('/profile/{id}/edit', [OneToOneController::class, 'edit'])->name('profile.edit'); // **এটি যোগ করতে হবে**
-
-// 6. ডেটা আপডেট করার জন্য রুট (Update)
 Route::put('/profile/{id}', [OneToOneController::class, 'update'])->name('profile.update');
 
-// 7. প্রোফাইল মুছে ফেলার জন্য রুট (Destroy)
-Route::delete('/profile/{id}', [OneToOneController::class, 'destroy'])->name('profile.destroy');
 
 
 
-// one to many
+// OneToMany relation
 
-Route::get('/documents', [PostCommentController::class, 'index'])->name('documents.index');
+Route::get('/documents', [OneToManyController::class, 'index'])->name('documents.index');
 
-Route::get('/comments', [PostCommentController::class, 'listComments'])->name('comments.index');
+Route::get('/documents/{document}/edit', [OneToManyController::class, 'edit'])->name('documents.edit');
 
-Route::get('/comments/{id}', [PostCommentController::class, 'show'])->name('comments.show');
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::put('/documents/{document}', [OneToManyController::class, 'update'])->name('documents.update');
 
 // HasOneThrough relation
 Route::get('/car', [HasOneThroughController::class, 'index']);
